cb5ad94bce70646ae065614b6e37eaf9ac0fd288
gosho-kazuya <gosho-kazuya@919.jp>
2016-11-12 12:16:49 +0900

    add whoami

--- backlog.go
+++ backlog.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	//	"json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+//	"os/exec"
 	"strings"
 
 	"github.com/motemen/ghq/utils"
 )
 
-const BACKLOG_ROOT_URL = "https://scat919.backlog.jp/api/v2/users/myself?apiKey="
+const BACKLOG_ROOT_URL = "https://scat919.backlog.jp/api/v2"
 
 // Convert url to issue key
 func GetIssueKeyFromURL(url_str string) string {
-	u, _ := url.Parse(url_str)
+	u, err := url.Parse(url_str)
+	utils.DieIf(err)
+
 	issue_key := strings.Split(u.Path, "/")
 	return issue_key[2]
 }
 
-func GetTitle(issue_key string) string {
-	resp, err := http.Get("https://scat919.backlog.jp/api/v2/users/myself?apiKey=" + api_key())
+func Whoami() string {
+	return curl("users/myself")
+}
+
+func api_key() string {
+	api_key := os.Getenv("BACKLOG_API_KEY")
+	if api_key == "" {
+		fmt.Println("please set BACKLOG_API_KEY")
+		os.Exit(1)
+	}
+
+	return api_key
+}
+
+func curl(url string) string{
+	resp, err := http.Get(fmt.Sprintf(
+		"%s/%s?apiKey=%s",
+		BACKLOG_ROOT_URL, url, api_key()))
+
 	utils.DieIf(err)
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -29,11 +50,7 @@ func GetTitle(issue_key string) string {
 	return string(body)
 }
 
-func api_key() string {
-	api_key := os.Getenv("BACKLOG_API_KEY")
-	if api_key == "" {
-		os.Exit(1)
-	}
-
-	return api_key
+func current_issue() string{
+	pwd, _ := os.Getwd()
+	return strings.Split(pwd, "/")[4]
 }
--- commands.go
+++ commands.go
@@ -6,10 +6,13 @@ import (
 	"os/exec"
 
 	"github.com/codegangsta/cli"
+	"github.com/motemen/ghq/utils"
 )
 
 var Commands = []cli.Command{
 	commandGet,
+	commandWhoami,
+	commandList,
 }
 
 var commandGet cli.Command = cli.Command{
@@ -19,16 +22,41 @@ var commandGet cli.Command = cli.Command{
 	Action: func(c *cli.Context) error {
 		issue_key := GetIssueKeyFromURL(c.Args().First())
 		clone_from := c.Args().First()
-		clone_to := os.Getenv("HOME") + "/.bhq/" + issue_key
+		clone_to := bhq_root() + "/" +  issue_key
 
 		fmt.Println("    ", clone_from, "->", clone_to)
 		_, err := exec.Command("mkdir", "-p", clone_to).Output()
 
-		fmt.Println(GetTitle(issue_key))
+//		fmt.Println(GetTitle(issue_key))
 
 		if err != nil {
 			fmt.Println(err)
 		}
 		return nil
 	},
+}
+var commandWhoami cli.Command = cli.Command{
+	Name:    "whoami",
+	Aliases: []string{"w"},
+	Usage:   "get backlog user",
+	Action: func(c *cli.Context) error {
+		fmt.Println(Whoami())
+		return nil
+	},
+}
+var commandList cli.Command = cli.Command{
+	Name:    "list",
+	Aliases: []string{"l"},
+	Usage:   "list backlog issues",
+	Action: func(c *cli.Context) error {
+		out, err := exec.Command("ls", bhq_root()).Output()
+		utils.DieIf(err)
+		fmt.Println(string(out))
+
+		return nil
+	},
+}
+
+func bhq_root() string{
+	return os.Getenv("HOME") + "/.bhq"
 }
